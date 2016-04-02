96a001d4625fb7d9802a6105a33803d185d9d0e7
Johann Dirry <Johann Dirry>
2016-04-02 16:04:11 +0200

    Refactoring Transition into an entity

--- ActiveStateMachine/Transitions/Transition.cs
+++ ActiveStateMachine/Transitions/Transition.cs
@@ -1,18 +1,12 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using Anotar.NLog;
-using MoreLinq;
-using NLog.Fluent;
 
 namespace ActiveStateMachine.Transitions
 {
-    public sealed class Transition : IObserver<string>
+    public sealed class Transition
     {
         public string Name { get; }
         public string SourceStateName { get; }
         public string TargetStateName { get; }
-        private bool IsCompleted { get; set; }
 
         /// <summary>
         /// List of checks that need to be done before the transition.
@@ -32,34 +26,6 @@ namespace ActiveStateMachine.Transitions
             TargetStateName = targetStateName;
             Preconditions = preconditions;
             TransitionActions = transitionActions;
-        }
-
-        [LogToErrorOnException]
-        public void OnNext(string value)
-        {
-            if (IsCompleted) return;
-
-            if (Preconditions.All(p => p.IsValid))
-            {
-                TransitionActions.ForEach(t => t.Execute());
-            }
-        }
-
-        public void OnError(Exception error)
-        {
-            if (IsCompleted) return;
-
-            Log.Trace()
-                .Message($"Handling error in transition {Name}.")
-                .Exception(error);
-
-            OnCompleted();
-        }
-        
-        [LogToErrorOnException]
-        public void OnCompleted()
-        {
-            IsCompleted = true;
         }
     }
 }
